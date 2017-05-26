4c31841b96c0a44aa85632149ac0fb3dc82ae0aa
James Lemire <webdev.jameslemire@gmail.com>
2017-05-26 13:28:33 -0600

    JL master - :construction:

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import Nav from './Nav/Nav';
 import Cart from './Cart/Cart';
 
+import router from '../router';
+
 export default function App() {
   return (
     <div>
       <Nav />
       <div id="App__mainContainer">
         <div id="App__routerContainer">
+          { router }
           <Cart />
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from './store';
 
 ReactDOM.render(
+  <BrowserRouter>
     <Provider store={ store }>
       <App />
-    </Provider>, 
+    </Provider>
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 registerServiceWorker();
