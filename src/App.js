d982a2ea05b103692d8496d887dfb65fada0ec4f
Beth Telford <mrsbethtelford@gmail.com>
2019-03-19 12:01:38 -0600

    final product

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { HashRouter } from "react-router-dom";
+import routes from "./routes";
+import Nav from './Components/Nav/Nav';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+          <Nav/>
+          {routes}
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
