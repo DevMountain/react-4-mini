bc93cb21439c4da270a39db250cfd41e72993a8b
zach <zaspringer@gmail.com>
2019-03-20 10:02:12 -0600

    moved files to make it work

---
+++ precache-manifest.42eff05c1d3d13038730fe709a1ec906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10e1c3bd49fbe06be46a",
+    "url": "/static/css/main.a9fe1c19.chunk.css"
+  },
+  {
+    "revision": "10e1c3bd49fbe06be46a",
+    "url": "/static/js/main.c3031a62.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a6ebea8b825161b9536",
+    "url": "/static/js/2.de6ac36f.chunk.js"
+  },
+  {
+    "revision": "911b7180a8b2b10ccd50c43f46fcd07a",
+    "url": "/static/media/dm_white_logo.911b7180.png"
+  },
+  {
+    "revision": "73052adad6f95ca8b63846659ca1788c",
+    "url": "/static/media/background.73052ada.png"
+  },
+  {
+    "revision": "6a6ab280b62995445e8059a70ccfe90b",
+    "url": "/index.html"
+  }
+];
